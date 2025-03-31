14728ed174f70bcc2346c14be306955ce254ce6f
Bagas Yadher Bima Nugraha Ainur Rofiq Hidayat <bagas.ybnarh@gmail.com>
2025-03-31 17:09:53 +0700

    Added dependencies and set up config

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,5 +1,63 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  app: {
+    head: {
+      link: [
+        {
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com",
+        },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Tomorrow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Host+Grotesk:ital,wght@0,300..800;1,300..800&display=swap",
+        },
+      ],
+    },
+  },
+  compatibilityDate: "2024-11-01",
+  debug: true,
+  devtools: { enabled: true },
+  kinde: {
+    authDomain: process.env.NUXT_KINDE_AUTH_DOMAIN,
+    clientId: process.env.NUXT_KINDE_CLIENT_ID,
+    clientSecret: process.env.NUXT_KINDE_CLIENT_SECRET,
+    logoutRedirectURL: process.env.NUXT_KINDE_LOGOUT_REDIRECT_URL,
+    redirectURL: process.env.NUXT_KINDE_REDIRECT_URL,
+    postLoginRedirectURL: process.env.NUXT_KINDE_POST_LOGIN_REDIRECT_URL,
+    password: process.env.NUXT_KINDE_PASSWORD,
+  },
+  hub: {
+    database: true,
+    blob: true,
+    kv: true,
+  },
+  modules: [
+    "@nuxtjs/kinde",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@primevue/nuxt-module",
+    "nuxt-viewport",
+    "nuxt-lodash",
+    "@nuxt/icon",
+    "@nuxthub/core",
+  ],
+  nitro: {
+    experimental: {
+      openAPI: true,
+    },
+  },
+  viewport: {
+    breakpoints: {
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+      xxl: 1536,
+    },
+  },
+});
